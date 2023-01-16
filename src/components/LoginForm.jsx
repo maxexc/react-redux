abef507028708cd4afc7bbaacc5435a2aab7fcca
Maksym Solomko <max.exc7@gmail.com>
2023-01-16 19:08:22 +0200

    update

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,12 +1,21 @@
+import { useDispatch } from "react-redux";
+import { logIn } from "redux/userSlice";
+
+
 
 export const LoginForm = () => {
+    const dispatch = useDispatch();
+
+
     const handleSubmit = e => {
         e.preventDefault();
         console.log(e.currentTarget.elements.login.value);
+        dispatch(logIn(e.currentTarget.elements.login.value));
         e.currentTarget.reset()
         //         or we can do like this:
         
         // const form = e.currentTarget;
+        // dispatch(logIn(form.elements.login.value));
         // form.reset();
     };
 
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+import {LoginForm} from 'components/LoginForm'
+
+export const LoginForm = () => {
+  
+
+    return (
+        <div>
+            <LoginForm />
+        </div>
+    );
+};
