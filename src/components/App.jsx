e618d6ab549d8f3d60ef22e7280449619e9674e2
Maksym Solomko <max.exc7@gmail.com>
2023-01-16 12:35:31 +0200

    base

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,37 @@
-export const App = () => {
+import React, { useState, useEffect } from 'react'
+import {useSelector, useDispatch} from 'react-redux';
+import { decrement, increment } from 'redux/store';
+
+
+const App = () => {
+  const dispatch = useDispatch();
+  const valueRedux = useSelector(state => state.myValue);
+  console.log(valueRedux);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101'
+        }}
+      >
+        React Redux Test
+      </div>
+      <div>
+          <nav>
+              {valueRedux}
+              <button onClick={() => dispatch(increment(100))}>Increment</button>
+              <button onClick={() => dispatch(decrement(50))}>Decrement</button>
+          </nav>
+      </div>
+      
+    </>    
   );
 };
+
+export default App
---
+++ src/redux/store.js
@@ -0,0 +1,74 @@
+import { configureStore  } from '@reduxjs/toolkit'
+import { createReducer, createAction, createSlice } from '@reduxjs/toolkit'
+
+// make slice
+const myValueSlice = createSlice({
+  name: "myValue", 
+  // initialState: {value: 100}, 
+  initialState: 150, 
+  reducers: {
+    increment(state, action) {
+      return state + action.payload
+      // state.value += action.payload;
+    },
+    decrement(state, action) {
+      return state - action.payload;
+    },
+  }
+});
+
+console.log(myValueSlice);
+
+export const { increment, decrement } = myValueSlice.actions;
+
+const itemsSlice = createSlice({
+  name: 'item',
+  initialState: [],
+  reducers: {
+    add(state, action) {
+      state.push(action.payload)
+    },
+    remove(state, action) {
+      return state.filter(item => item.id !== action.payload);
+    },
+  },
+})
+
+export const {add, remove} = itemsSlice.actions;
+
+// export const increment = createAction('myValue/increment');
+// export const decrement = createAction('myValue/decrement');
+
+// console.log(increment.toString());
+
+
+// const myReducer = createReducer(100, {
+//   [increment]: (state, action) => state + action.payload,
+//   [decrement]: (state, action) => state - action.payload,
+// });
+
+// 
+// export const add = createAction('items/add');
+// export const remove = createAction('items/remove')
+// // IMMER
+// const itemsReducer = createReducer([], {
+//   // [add]: (state, action) => [...state, action.payload,],
+//   [add]: (state, action) => state.push(action.payload),
+//   [remove]: (state, action) => state.filter(item => item.id !== action.payload),
+// });
+// // 
+
+export const store = configureStore({
+  reducer: {
+    myValue: myValueSlice.reducer,
+    items: itemsSlice.reducer,
+  },
+})
+
+// export const store = configureStore({
+//   reducer: {
+//     myValue: myReducer,
+//     // 
+//     items: itemsReducer,
+//   },
+// })
