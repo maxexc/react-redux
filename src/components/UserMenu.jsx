31b824fed35d003f95e626a135599cbbcb44c145
Maksym Solomko <max.exc7@gmail.com>
2023-01-17 00:40:14 +0200

    Update UserMenu.jsx

--- src/components/UserMenu.jsx
+++ src/components/UserMenu.jsx
@@ -4,19 +4,13 @@ import {logOut} from '../redux/userSlice'
 export const UserMenu = () => {
     const dispatch = useDispatch()
     const login = useSelector(state => state.user.login);
-    // const isLoggedIn = useSelector(state => state.user.isLoggedIn)
-    // if (login === '') {
-    //     return !isLoggedIn;
-    // }
 
     return (
     <div>
             {login} 
-            {/* {isLoggedIn &&  */}
             <button type='button' onClick={() => dispatch(logOut())}>
                 Log Out
             </button>
-            {/* } */}
     </div>
     )
 }; 
