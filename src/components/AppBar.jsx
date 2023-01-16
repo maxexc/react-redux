791f1e321ca6bdc8639f423fe2a5edf1f8a23026
Maksym Solomko <max.exc7@gmail.com>
2023-01-16 19:36:13 +0200

    update

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,17 +1,19 @@
-import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom"
-import { decrement, increment } from "redux/myValue/slice";
+import { UserMenu } from "./UserMenu";
 
 
 export const AppBar = () => {
 
     return (
         <header style={{
-            margin: '20px',        }}>
+            margin: '20px',
+            display: 'flex',
+            justifyContent: 'space-between',
+        }}>
         <nav>
-                <Link to="/login" style={{paddingRight: '20px' }}>Log in</Link>
-                
-        </nav>     
+            <Link to="/login" style={{paddingRight: '20px' }}>Log in</Link>
+        </nav>   
+        <UserMenu />
     </header>
     )
 }
