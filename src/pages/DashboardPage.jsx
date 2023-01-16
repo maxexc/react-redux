74d2af22d56d55e69493a3bcab9393808a998932
Maksym Solomko <max.exc7@gmail.com>
2023-01-17 00:20:35 +0200

    Update DashboardPage.jsx

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,3 +1,19 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom"; 
+import { useSelector } from 'react-redux'
+
+
+
+
+
 export const DashboardPage = () => {
+    const navigate = useNavigate();
+    const isLoggedIn = useSelector(state => state.user.isLoggedIn);
+    
+    useEffect(() => {
+        if(!isLoggedIn) {
+            navigate('/login', {replace: true})
+        }
+    });
     return <div>Dashboard Page</div>
 }
